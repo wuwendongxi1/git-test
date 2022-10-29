b8f2adf78ccec8511eb13782eef75baeea2576dc
Layne <Layne@atguigu.com>
2022-10-29 14:58:20 +0800

    first commit

---
+++ src/main/java/com/example/gittest/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.gittest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
